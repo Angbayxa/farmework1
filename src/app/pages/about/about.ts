9026e5631c90b05651ba54cefcfb4cca2905bb44
Pham Tuan Anh <01664157099hfgf@gmail.com>
2026-07-01 19:52:25 +0700

    LAP2

---
+++ src/app/pages/about/about.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  imports: [],
+  templateUrl: './about.html',
+  styleUrl: './about.css',
+})
+export class About {
+stk='123456789';
+sdt='0987654321';
+ten='nguyen van a';
+}
